72dcb01bf9a251f30e2a93addb12e6df303f59d2
berkaytuna <63649874+berkaytuna@users.noreply.github.com>
2021-03-12 16:48:21 +0100

    added read

--- serial/main4.cpp
+++ serial/main4.cpp
@@ -1,4 +1,3 @@
-
 #include <libusbp.hpp>
 #include <iostream>
 #include <iomanip>
@@ -9,8 +8,10 @@
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h> // write(), read(), close()
 
-const uint16_t vendor_id = 0x1a86;
-const uint16_t product_id = 0x7523;
+//const uint16_t vendor_id = 0x1a86;
+const uint16_t vendor_id = 0x0403;
+//const uint16_t product_id = 0x7523;
+const uint16_t product_id = 0x6001;
 const uint8_t interface_number = 0;
 const bool composite = true;
 
@@ -38,5 +39,42 @@ int main()
         printf("Error %i from open: %s\n", errno, strerror(errno));
     }
 
+    struct termios tty;
+    if(tcgetattr(serial_port, &tty) != 0) {
+        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+    }
+
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag |= CS8;
+    tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= CREAD | CLOCAL;
+    tty.c_lflag &= ~ICANON;
+    tty.c_lflag &= ~ECHO;
+    tty.c_lflag &= ~ECHOE;
+    tty.c_lflag &= ~ECHONL;
+    tty.c_lflag &= ~ISIG;
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL);
+    tty.c_oflag &= ~OPOST;
+    tty.c_oflag &= ~ONLCR;
+    tty.c_cc[VTIME] = 10;
+    tty.c_cc[VMIN] = 0;
+    cfsetispeed(&tty, B9600);
+    cfsetospeed(&tty, B9600);
+    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+    }
+
+    while (1) {
+        char read_buf [256];
+        int num_bytes = read(serial_port, &read_buf, sizeof(read_buf));
+        if (num_bytes < 0) {
+              printf("Error reading: %s", strerror(errno));
+              return 1;
+        }
+        else if (num_bytes == 13)
+            printf("Read %i bytes. Received message: %s", num_bytes, read_buf);
+    }
     return 0;
 }
