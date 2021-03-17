057de1de35dd5ed5d8899cff536c15e4579d85b0
berkaytuna <63649874+berkaytuna@users.noreply.github.com>
2021-03-17 22:20:48 +0100

    Add files via upload

---
+++ serverTest2/constants.h
@@ -0,0 +1,79 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <QCoreApplication>
+#include <QSettings>
+
+namespace cst {
+
+    // Size
+    static const struct {
+        int maxH = 300;
+        int maxW = 300;
+    } grid;
+
+    static const int settingsSize = 4;
+    static const int deviceSize = 2;
+
+    // Names
+
+    static const QVector <QString> tabNames = { "UI", "Card" };
+
+    static const struct {
+        QVector <QString> lines = { "Text", "Door number", "Initial number", "Interval" };
+        QVector <QString> checkBoxes = { "LFZeroFirst", "QRZeroFirst", "LFzeroInside",
+                                         "QRZeroInside", "LFTurnBytes", "QRTurnBytes",
+                                         "LFTurnBits", "QRTurnBits" };
+        QVector <QString> devices = { "LF", "QR" };
+    } labelNames;
+
+    /*static const struct {
+        QVector <QString> uiGeneral = { "Text" };
+        QVector <QString> uiDoor = { "Door number"};
+        QVector <QString> uiTime = { "Initial number", "Interval" };
+        QVector <QString> cardRemove = { "LF", "QR" };
+    } labelNames;*/
+
+    static const struct {
+        QVector <QString> ui = { "General", "Door", "Time" };
+        QVector <QString> card = { "Remove element", "Remove zero first", "Remove zero inside", "Turn bytes", "Turn bits within bytes" };
+    } groupNames;
+
+    // Reader
+    static const struct {
+        int readSize = 13;
+        int numberSize = 11;
+        QString friendlyName = "LF";
+        QString vid = "403";
+        QString pid = "6001";
+    } LF;
+
+    static const struct {
+        int readSize = 14;
+        int numberSize = 12;
+        QString friendlyName = "QR";
+        QString vid = "1a86";
+        QString pid = "7523";
+    } QR;
+
+    // Functions
+    static const QString getPath() {
+        QString path = QCoreApplication::applicationDirPath();
+        path.append("/config.ini");
+        return path;
+    };
+
+    static QVector <bool> readCheckBoxArr(QSettings *inSettings, const QString &deviceStr) {
+        QVector <bool> isCheckedArr;
+        int size = inSettings->beginReadArray(deviceStr);
+        for (int i = 0; i < size; ++i) {
+            inSettings->setArrayIndex(i);
+            isCheckedArr.push_back(inSettings->value(deviceStr).toBool());
+        }
+        inSettings->endArray();
+        return isCheckedArr;
+    };
+};
+
+
+#endif // CONSTANTS_H
